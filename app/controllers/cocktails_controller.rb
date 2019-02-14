f61c835a3162df12e9cec2de53bb0234a4c1d03a
Grace Mia <grace.mehrabe@gmail.com>
2019-02-14 12:51:24 +0100

    routes done

--- app/controllers/cocktails_controller.rb
+++ app/controllers/cocktails_controller.rb
@@ -1,2 +1,23 @@
 class CocktailsController < ApplicationController
+  before_action :set_cocktail, only: [:show, :edit, :update, :destroy]
+
+  def show
+    # ...
+  end
+  def edit
+    # ...
+  end
+  def update
+    # ...
+  end
+  def destroy
+    # ...
+  end
+
+  private
+
+  def set_cocktail
+    @cocktail = Cocktail.find(params[:id])
+  end
+
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,6 @@
+require 'open-uri'
+require 'json'
+
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
 #
@@ -5,3 +8,13 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+user_serialized = open('https://www.thecocktaildb.com/api/json/v1/1/list.php?i=list').read
+ingredient_list = JSON.parse(user_serialized)
+
+ingredient_list.each do |ingredient|
+  ingredient[1].each do |element|
+    Ingredient.create(name: element['strIngredient1'])
+  end
+  # Ingredient.create(ingredient.to_h)
+end
